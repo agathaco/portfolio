552bbbe7401a00cdc594cbf7fe094d75e1302d55
Agathe Cocco <agathecocco@gmail.com>
2020-09-14 15:14:06 +0200

    updated image path

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     {
