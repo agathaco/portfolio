e947eeb81c5cbb56f9c34971e27f9735504d52f5
Agathe Cocco <agathecocco@gmail.com>
2020-09-14 14:22:59 +0200

    typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,7 +37,7 @@ const IndexPage = () => {
           <div className="intro-text">
             <div className="tooltip intro-tooltip">Hi there!</div>
             <h1>I'm Agathe.</h1>
-            <h2>I'm a <span>software developer</span> and <span>contractor</span> with a soft spot for web design and animations.</h2>
+            <h2>I'm a <span>software developer</span> and <span>contractor</span> with a soft spot for web design and animation.</h2>
             <div className="divider"></div>
             <p>I specialise in JavaScript and have professional experience working with <span>React</span> and <span>Node</span>, but I'm also a big fan of <span>Vue</span>.<br/>
             In my spare time, I like to experiment with illustration and animation through code and create fun things like <a href="https://codepen.io/agathaco/full/NyQgQX">this</a>, <a href="https://codepen.io/agathaco/full/Reobbg">this</a> or <a href="https://codepen.io/agathaco/full/KKVLdgw">this</a>.<br/>
